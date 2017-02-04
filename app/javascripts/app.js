21aed0ff7478ab4ca0f5f0ead46415a3bd1da518
Mahesh Murthy <mahesh@signalhq.com>
2017-02-03 23:12:09 -0500

    Add some explanation in app.js

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -5,13 +5,20 @@ import "../stylesheets/app.css";
 import { default as Web3} from 'web3';
 import { default as contract } from 'truffle-contract'
 
-// Import our contract artifacts and turn them into usable abstractions.
+/*
+ * When you compile and deploy your Voting contract,
+ * truffle stores the abi and deployed address in a json
+ * file in the build directory. We will use this information
+ * to setup a Voting abstraction. We will use this abstraction
+ * later to create an instance of the Voting contract.
+ * Compare this against the index.js from our previous tutorial to see the difference
+ * https://gist.github.com/maheshmurthy/f6e96d6b3fff4cd4fa7f892de8a1a1b4#file-index-js
+ */
+
 import voting_artifacts from '../../build/contracts/Voting.json'
 
-// MetaCoin is our usable abstraction, which we'll use through the code below.
 var Voting = contract(voting_artifacts);
 
-let contractInstance = Voting.deployed();
 let candidates = {"Rama": "candidate-1", "Nick": "candidate-2", "Jose": "candidate-3"}
 
 window.voteForCandidate = function(candidate) {
@@ -20,6 +27,10 @@ window.voteForCandidate = function(candidate) {
     $("#msg").html("Vote has been submitted. The vote count will increment as soon as the vote is recorded on the blockchain. Please wait.")
     $("#candidate").val("");
 
+    /* Voting.deployed() returns an instance of the contract. Every call
+     * in Truffle returns a promise which is why we have used then()
+     * everywhere we have a transaction call
+     */
     Voting.deployed().then(function(contractInstance) {
       contractInstance.voteForCandidate(candidateName, {gas: 140000, from: web3.eth.accounts[0]}).then(function() {
         let div_id = candidates[candidateName];
@@ -36,7 +47,7 @@ window.voteForCandidate = function(candidate) {
 
 $( document ).ready(function() {
   if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source")
+    console.warn("Using web3 detected from external source like Metamask")
     // Use Mist/MetaMask's provider
     window.web3 = new Web3(web3.currentProvider);
   } else {
