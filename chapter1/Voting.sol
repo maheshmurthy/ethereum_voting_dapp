d259f59776bfed0292d3c2244a8f62deec3e7a18
Robert Meyer <robert.meyer@ni.tu-berlin.de>
2017-07-09 12:12:35 +0200

    Replaced throw by revert();
    `throw` is deprecated and, therefore, is replaced by the `revert()` function.

--- chapter1/Voting.sol
+++ chapter1/Voting.sol
@@ -19,12 +19,12 @@ contract Voting {
   }
 
   function totalVotesFor(bytes32 candidate) returns (uint8) {
-    if (validCandidate(candidate) == false) throw;
+    if (validCandidate(candidate) == false) revert();
     return votesReceived[candidate];
   }
 
   function voteForCandidate(bytes32 candidate) {
-    if (validCandidate(candidate) == false) throw;
+    if (validCandidate(candidate) == false) revert();
     votesReceived[candidate] += 1;
   }
 
