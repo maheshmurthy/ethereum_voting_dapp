0b84aefb08921a97197b0fbbbbf2a44af086cc84
Robert Meyer <robert.meyer@ni.tu-berlin.de>
2017-07-10 16:29:37 +0200

    Replaced conditional revert with require

--- chapter1/Voting.sol
+++ chapter1/Voting.sol
@@ -19,12 +19,12 @@ contract Voting {
   }
 
   function totalVotesFor(bytes32 candidate) returns (uint8) {
-    if (validCandidate(candidate) == false) revert();
+    require(validCandidate(candidate));
     return votesReceived[candidate];
   }
 
   function voteForCandidate(bytes32 candidate) {
-    if (validCandidate(candidate) == false) revert();
+    require(validCandidate(candidate));
     votesReceived[candidate] += 1;
   }
 
